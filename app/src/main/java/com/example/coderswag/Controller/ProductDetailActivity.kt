76cbd709fa1b783d1611ca57f063ad324eee6638
Jenifer Martin <jenifer.martin@vml.com>
2019-04-01 22:19:37 -0500

    added data binding to details activity and toast to buy button

--- app/src/main/java/com/example/coderswag/Controller/ProductDetailActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/ProductDetailActivity.kt
@@ -2,6 +2,9 @@ package com.example.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.coderswag.Model.Product
 import com.example.coderswag.R
 import com.example.coderswag.Services.DataService
 import com.example.coderswag.Utilities.EXTRA_CATEGORY
@@ -11,6 +14,8 @@ import kotlinx.android.synthetic.main.product_list_item.*
 
 class ProductDetailActivity : AppCompatActivity() {
 
+    lateinit var selectedProduct : Product
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
@@ -18,14 +23,14 @@ class ProductDetailActivity : AppCompatActivity() {
         val selectedProductTitle = intent.getStringExtra(EXTRA_PRODUCT)
         val selectedCategoryTitle = intent.getStringExtra(EXTRA_CATEGORY)
 
-        val selectedProduct = DataService.getSelectedProduct(selectedCategoryTitle, selectedProductTitle)
+        selectedProduct = DataService.getSelectedProduct(selectedCategoryTitle, selectedProductTitle)
         val resourceId = resources.getIdentifier(selectedProduct.image, "drawable", packageName)
-//        selectedProductImage.setImageResource(resourceId)
-//        selectedProductName.text = selectedProduct.title
-//        selectedProductDescription.text = selectedProduct.price
+        selectedProductDetailName.text = selectedProduct.title
+        selectedProductDetailImage.setImageResource(resourceId)
+        selectedProductDetailDescription.text = "this ${selectedProduct.title} can be yours for ${selectedProduct.price}"
+    }
 
-
-
-
+    fun buyButtonClicked(view: View) {
+        Toast.makeText(this, "You added a ${selectedProduct.title} to your cart for ${selectedProduct.price}", Toast.LENGTH_LONG).show()
     }
 }
